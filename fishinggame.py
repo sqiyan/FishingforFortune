471680d082d8d929e5af20f5b6b92ced04330a80
sqiyan <60425964+sqiyan@users.noreply.github.com>
2020-04-15 16:06:56 +0800

    Add files via upload

---
+++ fishinggame.py
@@ -0,0 +1,27 @@
+
+
+def introduction():
+	print("""
+	In a quiet old town away from the city, John lives in his little shack by the sea,
+	letting the days go by. The economy isn't doing too good these days, and John, who 
+	hasn't had the best education, remains unemployed and unmotivated. 
+
+	In his mid 20s, John is in his prime age, but yet it is wasted away in his poverty. 
+	He is a simple man with a simple wish: to move out of his run-down little house
+	and have a proper home.
+
+	Just when it all seemed hopeless for John, one day, he found an old fishing rod washed
+	up on shore. 
+
+	Maybe there was hope for him afterall.
+
+	HOW TO PLAY:
+	You are John. Well, John is you. And all you have to do, is fish for your fortune.
+
+	FISHING
+
+
+	""")
+	return
+
+introduction()
